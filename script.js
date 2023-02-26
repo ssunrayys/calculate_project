eb3d8463a1afd05de0cb8d4827fc3c8944a7e00a
Anna Bondar <Annabondar.pm@gmail.com>
2023-02-26 20:47:55 +0200

    add files

---
+++ script.js
@@ -0,0 +1,22 @@
+let a = +prompt("Write first number, please:");
+let b = +prompt("Write second number, please:");
+
+function sum(a, b) {
+	alert(a + b);
+}
+sum(a, b);
+
+function minus(a, b) {
+	alert(a - b);
+}
+minus(a, b);
+
+function multiply(a, b) {
+	alert(a * b);
+}
+multiply(a, b);
+
+function divide(a, b) {
+    alert(a / b);
+}
+divide(a, b);
